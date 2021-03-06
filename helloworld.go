01e2d547d09b24ab440841d9f4756c1cc58f4ee9
Maciej <maciej@contact.apurer.io>
2021-03-06 19:32:39 +0100

    testing db connection

--- helloworld.go
+++ helloworld.go
@@ -9,9 +9,70 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+
+	var (
+		dbUser    = os.Getenv("DB_USER") // e.g. 'my-db-user'
+		dbPwd     = os.Getenv("DB_PASS") // e.g. 'my-db-password'
+		dbTCPHost = os.Getenv("DB_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+		dbPort    = os.Getenv("DB_PORT") // e.g. '3306'
+		dbName    = os.Getenv("DB_NAME") // e.g. 'my-database'
+	)
+
+	if dbUser == "" {
+		log.Fatal("DB_USER environment variable must be set")
+	}
+
+	if dbPwd == "" {
+		log.Fatal("DB_PASS environment variable must be set")
+	}
+
+	if dbTCPHost == "" {
+		log.Fatal("DB_HOST environment variable must be set")
+	}
+
+	if dbPort == "" {
+		log.Fatal("DB_PORT environment variable must be set")
+	}
+
+	if dbName == "" {
+		log.Fatal("DB_NAME environment variable must be set")
+	}
+
+	var dbURI string
+	dbURI = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPwd, dbTCPHost, dbPort, dbName)
+
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("mysql", dbURI)
+	if err != nil {
+		fmt.Printf("sql.Open: %v", err)
+		return
+	}
+
+	results, err := dbPool.Query("SELECT greeting FROM hello")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	var herro string
+
+	for results.Next() {
+
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&herro)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		log.Printf(herro)
+	}
+
 	http2ProxyPort := os.Getenv("HTTP2_SERVER_PORT")
 	if http2ProxyPort == "" {
 		log.Fatal("HTTP2_SERVER_PORT environment variable must be set")
@@ -50,7 +111,7 @@ func main() {
 				log.Println(err)
 				continue
 			}
-			go handleConnection(conn)
+			go handleConnection(conn, herro)
 		}
 	}()
 
@@ -70,6 +131,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
+
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Scheme: %s\n", r.URL.Scheme)
 	fmt.Fprintf(w, "Protocol: %s!\n", r.Proto)
@@ -85,10 +147,10 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, herro string) {
 	defer conn.Close()
 
-	hello := "hello there "
+	hello := fmt.Sprintf("%s ", herro)
 
 	for {
 		time.Sleep(1 * time.Second)
